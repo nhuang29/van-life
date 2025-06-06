337d2a38024cac88180c85d04eb29f51438e0028
Nicholas Huang <nickhuang29@gmail.com>
2025-06-06 13:05:53 -0400

    The Van Detail page and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import Home from './pages/Home';
 import About from './pages/About';
+import Vans from './pages/Vans';
+import VanDetail from './pages/VanDetail';
 import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import "./server";
 
 function App() {
 
@@ -11,11 +14,14 @@ function App() {
         <Link className="site-logo" to="/">#VanLife</Link>
         <nav>
           <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
+        <Route path="/vans/:id" element={<VanDetail />} />
       </Routes>
     </BrowserRouter>
   )
